4be5a45022b86091497aa68b06b868cd34981244
MePsyDuck <sumit231996@gmail.com>
2018-01-21 16:55:38 +0530

    added serializer objects

--- server/api/serializers/__init__.py
+++ server/api/serializers/__init__.py
@@ -4,3 +4,10 @@ from .letter import LetterSerializer
 from .remark import RemarkSerializer
 from .student import StudentSerializer
 from .user import UserSerializer
+
+faculty_serializer = FacultySerializer()
+inbox_serializer = InboxSerializer()
+letter_serializer = LetterSerializer()
+remark_serializer = RemarkSerializer()
+student_serializer = StudentSerializer()
+user_serializer = StudentSerializer()
