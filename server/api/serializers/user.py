91e2f63ac7a0532a9ca5bfca273bbd0fbdfd0f76
MePsyDuck <sumit231996@gmail.com>
2018-02-10 17:46:05 +0530

    profile id serialised

--- server/api/serializers/user.py
+++ server/api/serializers/user.py
@@ -4,6 +4,21 @@ from api.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    student = serializers.SerializerMethodField()
+    faculty = serializers.SerializerMethodField()
+
     class Meta:
         model = User
         fields = '__all__'
+
+    def get_student(self, user):
+        if user.is_student():
+            return user.student.id
+        else:
+            return
+
+    def get_faculty(self, user):
+        if user.is_faculty():
+            return user.faculty.id
+        else:
+            return
